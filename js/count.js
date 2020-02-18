210d3aafbe9bd6c90452fcaf0c0d4fb39d585dfa
lmw <lmwfjb@gmail.com>
2020-02-18 10:30:42 +0800

    shopping cart  count fixed

--- js/count.js
+++ js/count.js
@@ -2,6 +2,9 @@ let price1 = parseInt($(".price1").text());
 let price2 = parseInt($(".price2").text());
 let price3 = parseInt($(".price3").text());
 
+let p_price = parseInt($(".p_price").text());
+let t_price = parseInt($(".t_price").text());
+
 $(".qty-input div").click(function () {
     let input_value1 = parseInt($(".input1").val());
     let input_value2 = parseInt($(".input2").val());
@@ -11,8 +14,7 @@ $(".qty-input div").click(function () {
     let fprice2 = parseInt($(".fprice2").text());
     let fprice3 = parseInt($(".fprice3").text());
 
-    let p_price = parseInt($(".p_price").text());
-    let t_price = parseInt($(".t_price").text());
+    
 
     fprice1 = input_value1 * price1;
     fprice2 = input_value2 * price2;
@@ -24,5 +26,27 @@ $(".qty-input div").click(function () {
     $(".fprice3").text(fprice3);
     $(".p_price").text(p_price);
     $(".t_price").text(t_price);
+})
 
+$(".delete1").click(function(){
+    let dprice1 = parseInt($(".fprice1").text());
+    p_price = p_price-dprice1;
+    t_price = t_price-dprice1
+    $(".p_price").text(p_price);
+    $(".t_price").text(t_price);
+})
+
+$(".delete2").click(function(){
+    let dprice2 = parseInt($(".fprice2").text());
+    p_price = p_price-dprice2;
+    t_price = t_price-dprice2;
+    $(".p_price").text(p_price);
+    $(".t_price").text(t_price);
+})
+$(".delete3").click(function(){
+    let dprice3 = parseInt($(".fprice3").text());
+    p_price = p_price-dprice3;
+    t_price = t_price-dprice3;
+    $(".p_price").text(p_price);
+    $(".t_price").text(t_price);
 })
